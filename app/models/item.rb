0b7bdf18bb90571e6d155ec162c245b16882b479
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-01-13 17:13:41 +0900

    バリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,15 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one :genre
   has_one_attached :image
+
+  with_options presence: true do
+    validates :title 
+    validates :explanation
+    validates :price
+    validates :category_id
+    validates :item_status_id
+    validates :burden_id
+    validates :prefecture_id
+    validates :day_id
+  end
 end
