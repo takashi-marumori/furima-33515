4fdeacf9aa7a3acef9ad0f2dda594be94eeda5c6
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-01-13 17:30:46 +0900

    new,createアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item =Item.create(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:title, :explanation,:price,:category_id,:item_status_id,
+      :burden_id,:prefecture_id,:day_id,:image).merge(user_id: current_user.id)
+  end
 end
