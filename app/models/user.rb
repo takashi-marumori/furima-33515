41e04d9954d348722df8621276fa6fe9557d0c69
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-01-13 09:45:23 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+         validates :nickname, presence: true 
+         validates :first_name, format: {with:/\A[ぁ-んァ-ン一-龥]/ , message: "には漢字、カナ、かなのどれかでの入力が必要です。", presence: true}
+         validates :last_name, format: {with:/\A[ぁ-んァ-ン一-龥]/ , message: "には漢字、カナ、かなのどれかでの入力が必要です。", presence: true}
+         validates :read_first_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "にはカタカナでの入力が必要です。", presence: true}
+         validates :read_last_name, format: { with: /\A[ァ-ヶー－]+\z/, message: "にはカタカナでの入力が必要です。", presence: true}
+         validates :date, presence: true
+         validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message: "には半角英数字が必要です。"}
 end
