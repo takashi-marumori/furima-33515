29c69932b1e649e1eb708111b7adf3808090b885
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-01-13 19:38:24 +0900

    アクティブハッシュ内にワードを記述、アクティブハッシュに対するバリデーションを設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,17 +3,23 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   extend ActiveHash::Associations::ActiveRecordExtensions
-  validates :category_id, numericality: { other_than: 1 }
+  belongs_to :category
+  belongs_to :item_status
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :day
 
   with_options presence: true do
     validates :title 
     validates :explanation
     validates :price,format: { with:/\A[0-9]+\z/},length: { in:300..9999999 }
-    validates :category_id
-    validates :item_status_id
-    validates :burden_id
-    validates :prefecture_id
-    validates :day_id
-    validates :image, presence: true
+    validates :image
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :item_status_id
+      validates :burden_id
+      validates :prefecture_id
+      validates :day_id
+    end
   end
 end
