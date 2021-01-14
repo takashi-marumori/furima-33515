ba36b144fbe4f9f04b79dadee5563ced81fa5094
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-01-14 17:50:38 +0900

    バリデーション修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -10,9 +10,10 @@ class Item < ApplicationRecord
   belongs_to :day
 
   with_options presence: true do
-    validates :title 
+    validates :title
     validates :explanation
-    validates :price,format: { with:/\A[0-9]+\z/},numericality: { :greater_than_or_equal_to =>300,:less_than_or_equal_to => 9999999 }
+    validates :price, format: { with: /\A[0-9]+\z/ },
+                      numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
     validates :image
     with_options numericality: { other_than: 1 } do
       validates :category_id
