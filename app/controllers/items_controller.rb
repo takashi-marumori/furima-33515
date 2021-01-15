56d9982d6c75b474149f0c91a69c8dcd967be4aa
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-01-15 18:43:18 +0900

    authenticate_user!のアクションを追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: :new
+  before_action :authenticate_user!, only: [:new, :edit, :update]
   before_action :set_item, only: [:show, :edit, :update]
 
   def index
