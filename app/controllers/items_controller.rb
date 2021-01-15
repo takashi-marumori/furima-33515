e12d7d848e3fe7393c9683ac6399381a86132f47
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-01-15 21:35:33 +0900

    destroyアクションを修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -35,10 +35,8 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user_id == current_user.id
-      @item.destroy
-      redirect_to root_path
-    end
+    @item.destroy if @item.user_id == current_user.id
+    redirect_to root_path
   end
 
   private
