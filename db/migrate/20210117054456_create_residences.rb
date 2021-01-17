368e336e6b5f2f4f4cf23deb972faa83f67353b2
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-01-17 15:00:24 +0900

    カラム追加

--- db/migrate/20210117054456_create_residences.rb
+++ db/migrate/20210117054456_create_residences.rb
@@ -1,7 +1,13 @@
 class CreateResidences < ActiveRecord::Migration[6.0]
   def change
     create_table :residences do |t|
-
+      t.string     :postal_code,   null: false
+      t.integer    :prefecture_id, null: false
+      t.string     :municipality,  null: false
+      t.string     :address,       null: false
+      t.string     :building_name
+      t.string     :phone_number,  null:false
+      t.references :order,         foreign_key: true
       t.timestamps
     end
   end
