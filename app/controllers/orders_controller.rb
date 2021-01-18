b64b9c1c67ce2b1fa0284560483431564c06b87c
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-01-18 18:50:49 +0900

    商品購入画面の更新

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,9 @@
 class OrdersController < ApplicationController
-
   def index
+    @items = Item.find(params[:item_id])
+    @user_order = UserOrder.new
   end
 
+  def create
+  end
 end
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -1,20 +1,20 @@
-class user_order
+class UserOrder
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number
-  
+
   with_options presence: true do
-  validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
-  validates :municipality
-  validates :address
-  validates :phone_number, format: { with: ^0\d{9,10}$,^0\d{2,3}-\d{1,4}-\d{4}$ }
+    validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :municipality
+    validates :address
+    validates :phone_number, format: { with: /\A0\d{2,5}-?\d{1,4}-?\d{1,4}\z/ },
+                             numericality: { greater_than_or_equal_to: 10, less_than_or_equal_to: 11 }
   end
 
   validates :prefecture, numericality: { other_than: 1 }
 
   def save
-    Residence.create(postal_code: postal_code,prefecture_id: prefecture_id,municipality: municipality,address: address, building_name: building_name,phone_number: phone_number)
-    Order.create(user_id: user.id,item_id: item.id)
+    Residence.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address,
+                     building_name: building_name, phone_number: phone_number)
+    Order.create(user_id: user.id, item_id: item.id)
   end
-
-
 end
