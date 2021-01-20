632ab4fad6b95ed19e4c813951f641ae72bacaf3
takashi-marumori <76422764+takashi-marumori@users.noreply.github.com>
2021-01-20 12:40:01 +0900

    DBに保存できるように修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -19,14 +19,14 @@ class OrdersController < ApplicationController
   private
 
   def user_order_params
-    params.permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number,
-                                  :price).merge(token: params[:token])
+    params.require(:user_order).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number,
+                                  ).merge(user_id: current_user.id, item_id: @items.id,token: params[:token])
   end
 
   def pay_item
     Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
-      amount: user_order_params[:price],
+      amount: @items.price,
       card: user_order_params[:token],
       currency: 'jpy'
     )
--- app/models/user_order.rb
+++ app/models/user_order.rb
@@ -5,15 +5,14 @@ class UserOrder
   with_options presence: true do
     validates :token
     validates :postal_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/ }
+    validates :prefecture_id, numericality: { other_than: 1 }
     validates :municipality
     validates :address
     validates :phone_number, format: { with: /\A\d{1,11}\z/ }
   end
 
-  validates :prefecture_id, numericality: { other_than: 1 }
-
   def save
-    order = Order.create(user_id: user.id, item_id: item.id)
+    order = Order.create(user_id: user_id, item_id: item_id)
     Residence.create(postal_code: postal_code, prefecture_id: prefecture_id, municipality: municipality, address: address,
                      building_name: building_name, phone_number: phone_number, order_id: order.id)
   end
